908327ff9d1fbf021a8549301647737120e4b892
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-05-20 11:33:57 +0530

    template-create

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Welcome Simple Todo List...!"
+    # return "Welcome Simple Todo List...!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
